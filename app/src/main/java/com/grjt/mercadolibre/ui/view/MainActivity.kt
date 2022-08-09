4c859be18a4f4160784bb31109b18b69e7d1acbb
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2022-08-08 20:06:04 -0500

    Implement Search MVVM, UseCase

--- app/src/main/java/com/grjt/mercadolibre/ui/view/MainActivity.kt
+++ app/src/main/java/com/grjt/mercadolibre/ui/view/MainActivity.kt
@@ -2,11 +2,44 @@ package com.grjt.mercadolibre.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.lifecycle.Observer
 import com.grjt.mercadolibre.R
+import com.grjt.mercadolibre.ui.viewmodel.ItemViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val itemViewModel : ItemViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        itemViewModel.onSearch("Motorola%20G6")
+
+        itemViewModel.itemModel.observe(this, Observer { result ->
+            Log.d("resultVM", "${result.results.size}")
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val menuItem = menu?.findItem(R.id.action_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.queryHint = "Ingresa el artículo a buscar"
+        searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                itemViewModel.onSearch(query)
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
